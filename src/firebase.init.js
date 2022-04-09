1dcc52f3cab8445b312e11a8593628e55d75c325
Raselraaz123 <raselraaz91@gmail.com>
2022-04-09 14:53:11 +0600

    first commit

---
+++ src/firebase.init.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIeI4Qf9ZWhFEfBQekph15XtmCP0se2YI",
+  authDomain: "website-with-react-router.firebaseapp.com",
+  projectId: "website-with-react-router",
+  storageBucket: "website-with-react-router.appspot.com",
+  messagingSenderId: "729783595838",
+  appId: "1:729783595838:web:63b08d712dc71b16e1726f",
+};
+
+
+const app = initializeApp(firebaseConfig);
+export default app;
